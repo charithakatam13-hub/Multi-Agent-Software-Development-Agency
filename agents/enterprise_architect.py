347efdab89198fec0dcaacd954270296470822cb
Katam Sai Charitha <charithakatam13@gmail.com>
2026-06-12 12:22:07 +0530

    Create enterprise_architect.py

---
+++ agents/enterprise_architect.py
@@ -0,0 +1,19 @@
+def enterprise_architect(state):
+
+    return {
+        "architecture_plan": """
+Enterprise Architecture
+
+Tech Stack:
+- Java Spring Boot
+- Microservices
+- Kafka
+- Docker
+
+Modules:
+- User Management
+- ERP Core
+- Reporting
+- Notification Service
+"""
+    }
