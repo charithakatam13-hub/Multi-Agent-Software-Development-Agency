5d9f17ffd9321597b80fe0914def5868d801a963
Katam Sai Charitha <charithakatam13@gmail.com>
2026-06-12 12:21:38 +0530

    Create frontend_architect.py

---
+++ agents/frontend_architect.py
@@ -0,0 +1,18 @@
+def frontend_architect(state):
+
+    return {
+        "architecture_plan": """
+Web Architecture
+
+Tech Stack:
+- React
+- Next.js
+- Tailwind CSS
+
+Modules:
+- Home Page
+- Product Catalog
+- User Dashboard
+- Checkout
+"""
+    }
